f04fc28dad56efaf542dcc3ffba3b398b5c19276
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-01-30 16:56:19 -0500

    Added the "ForbidenDisplayName" enum.

---
+++ Enums/ForbidenDisplayName.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace NuJournalPro.Enums
+{
+    public enum ForbidenDisplayName
+    {
+        [Description("Owner")]
+        Owner,
+        [Description("Administrator")]
+        Administrator,
+        [Description("Editor")]
+        Editor,
+        [Description("Author")]
+        Author,
+        [Description("Contributor")]
+        Contributor,
+        [Description("Moderator")]
+        Moderator,
+        [Description("Reader")]
+        Reader,
+        [Description("Restricted")]
+        Restricted,
+        [Description("Banned")]
+        Banned,
+        [Description("Manager")]
+        Manager,
+        [Description("Support")]
+        Support,
+        [Description("Staff")]
+        Staff,
+        [Description("NuJournal")]
+        NuJournal,
+        [Description("Admin")]
+        Admin
+    }
+}
