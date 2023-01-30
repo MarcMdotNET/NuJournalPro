67db5aa77a6c5694928bde0dd57b633fd07c8079
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-01-30 16:25:05 -0500

    Added the "PostImage" data model.

---
+++ Models/Database/PostImage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Hosting;
+using NuJournalPro.Models.Media;
+
+namespace NuJournalPro.Models.Database
+{
+    public class PostImage : CompressedImage
+    {
+        public int Id { get; set; }
+        public int PostId { get; set; }
+        public string? NuJournalUserId { get; set; }
+
+        // Database Navigation Properties
+        public virtual Post? Post { get; set; }
+        public virtual NuJournalUser? NuJournalUser { get; set; }
+    }
+}
