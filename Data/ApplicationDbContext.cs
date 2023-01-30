8fb8aa615d25663c323e3d39209dfc8212c8f829
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-01-30 17:11:25 -0500

    Updated the "ApplicationDbContext" constructor.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using NuJournalPro.Models.Database;
 
 namespace NuJournalPro.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<NuJournalUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<Page>? Pages { get; set; }
+        public DbSet<Blog>? Blogs { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<Comment>? Comments { get; set; }
+        public DbSet<Tag>? Tags { get; set; }
+        public DbSet<PageImage>? PageImage { get; set; }
+        public DbSet<BlogImage>? BlogImage { get; set; }
+        public DbSet<PostImage>? PostImage { get; set; }
+        public DbSet<ProfilePicture>? ProfilePicture { get; set; }
     }
 }
