723bd9e1f89c33553da67723aede2814088f2b7f
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-01-30 16:22:10 -0500

    Added the "PageImage" data model.

---
+++ Models/Database/PageImage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using NuJournalPro.Models.Media;
+
+namespace NuJournalPro.Models.Database
+{
+    public class PageImage : CompressedImage
+    {
+        public int Id { get; set; }
+        public int PageId { get; set; }
+        public string? NuJournalUserId { get; set; }
+
+        // Database Navigation Properties
+        public virtual Page? Page { get; set; }
+        public virtual NuJournalUser? NuJournalUser { get; set; }
+    }
+}
