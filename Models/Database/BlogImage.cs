68dbad8fc61e550806da1478b6d9315f45b2d742
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-01-30 16:23:41 -0500

    Added the "BlogImage" data model.

---
+++ Models/Database/BlogImage.cs
@@ -0,0 +1,16 @@
+﻿using NuJournalPro.Models.Media;
+using System.Reflection.Metadata;
+
+namespace NuJournalPro.Models.Database
+{
+    public class BlogImage : CompressedImage
+    {
+        public int Id { get; set; }
+        public int BlogId { get; set; }
+        public string? NuJournalId { get; set; }
+
+        // Database Navigation Properties
+        public virtual Blog? Blog { get; set; }
+        public virtual NuJournalUser? NuJournalUser { get; set; }
+    }
+}
