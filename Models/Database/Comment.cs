de95dfe02734afb297598ca124eb0c9a18d17ae5
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-01-30 16:41:07 -0500

    Added the "ModerationType" enum.

--- Models/Database/Comment.cs
+++ Models/Database/Comment.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using NuJournalPro.Enums;
+using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 
 namespace NuJournalPro.Models.Database
