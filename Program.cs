9642fc8b16e92beed3070715f1eab4c7208529bc
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-01-30 16:03:03 -0500

    Transitioned from Microsoft SQL to PostgreSQL.

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ using NuJournalPro.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+var connectionString = builder.Configuration.GetSection("pgsqlSettings")["pgsqlConnection"];
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
