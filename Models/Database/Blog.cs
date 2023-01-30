7790521be24a8b51909979ead3bdf6119f9399d7
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-01-30 16:31:56 -0500

    Added the "Blog" data model.

---
+++ Models/Database/Blog.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace NuJournalPro.Models.Database
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string? NuJournalUserId { get; set; }
+
+        [Required]
+        [Display(Name = "Title")]
+        [StringLength(256, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Description")]
+        [StringLength(1024, ErrorMessage = "The {0} must be at least {2} and at most {1} characters lonf.", MinimumLength = 2)]
+        public string Description { get; set; } = string.Empty;
+
+        [Display(Name = "Created")]
+        [DataType(DataType.DateTime)]
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        [Display(Name = "Modified")]
+        [DataType(DataType.DateTime)]
+        public DateTime? Modified { get; set; }
+
+        // Database Navigation Properties
+        [Display(Name = "Author")]
+        public virtual NuJournalUser? NuJournalUser { get; set; }
+        public virtual BlogImage? BlogImage { get; set; }
+        public virtual ICollection<Post> Posts { get; set; } = new HashSet<Post>();
+    }
+}
