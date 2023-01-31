875db5786d9a2e1d5e61f27cdc6e09c2d6bdea65
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-01-31 16:53:40 -0500

    Scaffolded the "PagesController" with views.

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NuJournalPro.Data;
+using NuJournalPro.Models.Database;
+
+namespace NuJournalPro.Controllers
+{
+    public class PagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pages
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Pages.Include(p => p.NuJournalUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Pages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Pages == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.Pages
+                .Include(p => p.NuJournalUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        // GET: Pages/Create
+        public IActionResult Create()
+        {
+            ViewData["NuJournalUserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Pages/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NuJournalUserId,Title,Abstract,Content,Created,Modified,PageStatus,PageVisibility,Slug")] Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(page);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NuJournalUserId"] = new SelectList(_context.Users, "Id", "Id", page.NuJournalUserId);
+            return View(page);
+        }
+
+        // GET: Pages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Pages == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.Pages.FindAsync(id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+            ViewData["NuJournalUserId"] = new SelectList(_context.Users, "Id", "Id", page.NuJournalUserId);
+            return View(page);
+        }
+
+        // POST: Pages/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NuJournalUserId,Title,Abstract,Content,Created,Modified,PageStatus,PageVisibility,Slug")] Page page)
+        {
+            if (id != page.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(page);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PageExists(page.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NuJournalUserId"] = new SelectList(_context.Users, "Id", "Id", page.NuJournalUserId);
+            return View(page);
+        }
+
+        // GET: Pages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Pages == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.Pages
+                .Include(p => p.NuJournalUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        // POST: Pages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Pages == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Pages'  is null.");
+            }
+            var page = await _context.Pages.FindAsync(id);
+            if (page != null)
+            {
+                _context.Pages.Remove(page);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PageExists(int id)
+        {
+          return _context.Pages.Any(e => e.Id == id);
+        }
+    }
+}
