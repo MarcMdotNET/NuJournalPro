cc934ad0ada975d2e18fb5ac149c5a0cec6a7437
marcm-pro <100318111+marcm-pro@users.noreply.github.com>
2023-02-01 16:23:30 -0500

    Added several user control panel administration pages.

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -9,7 +9,14 @@ namespace  NuJournalPro.Areas.Identity.Pages.Account.Manage
 {
     public static class ManageNavPages
     {
+        public static string OwnerPanel => "OwnerPanel";
+        
+        public static string AdminPanel => "AdminPanel";
         public static string Index => "Index";
+
+        public static string SocialMedia => "SocialMedia";
+
+        public static string ProfilePicture => "ProfilePicture";
 
         public static string Email => "Email";
 
@@ -25,7 +32,15 @@ namespace  NuJournalPro.Areas.Identity.Pages.Account.Manage
 
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
+        public static string OwnerPanelNavClass(ViewContext viewContext) => PageNavClass(viewContext, OwnerPanel);
+
+        public static string AdminPanelNavClass(ViewContext viewContext) => PageNavClass(viewContext, AdminPanel);
+
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+
+        public static string SocialMediaNavClass(ViewContext viewContext) => PageNavClass(viewContext, SocialMedia);
+
+        public static string ProfilePictureNavClass(ViewContext viewContext) => PageNavClass(viewContext, ProfilePicture);
 
         public static string EmailNavClass(ViewContext viewContext) => PageNavClass(viewContext, Email);
 
